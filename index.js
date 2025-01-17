aa85dddd575fec8bd035e27872fcd36376f54e1e
Nabila Islam <nabilaislamhstu53@gmail.com>
2025-01-17 21:41:22 +0600

    jwt rout and verification middleware

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mngo1.mrkjr.mongodb.net/?retryWrites=true&w=majority&appName=mngo1`;
 
+//token verification
+ // middlewares 
+ const verifyToken = (req, res, next) => {
+  
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -33,6 +50,12 @@ async function run() {
 
 
 // ================================================================================= User Data=======
+// jwt
+app.post("/jwt",async(req,res)=>{
+  const user=req.body;
+  const token=jwt.sign(user,process.env.TOKEN_SECRET,{expiresIn:"24h"});
+  res.send({token});
+})
 app.post("/users",async(req,res)=>{
     const user=req.body;
     const query={email:user.email};
